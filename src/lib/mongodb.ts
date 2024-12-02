83ac0e00c1c7943f4295f951e53b5fba1fa4b8b9
Alex <alex@devarno.com>
2024-12-02 20:37:07 +0000

    mongodb.ts

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -5,26 +5,26 @@ if (!process.env.MONGODB_URI) {
 }
 
 const uri = process.env.MONGODB_URI;
+const options = {};
 
 let client: MongoClient;
 
 if (process.env.NODE_ENV === "development") {
     // In development mode, use a global variable so that the value
     // is preserved across module reloads caused by HMR (Hot Module Replacement).
-    const globalWithMongo = global as typeof globalThis & {
+    let globalWithMongo = global as typeof globalThis & {
         _mongoClient?: MongoClient;
     };
 
     if (!globalWithMongo._mongoClient) {
-        globalWithMongo._mongoClient = new MongoClient(uri);
+        globalWithMongo._mongoClient = new MongoClient(uri, options);
     }
     client = globalWithMongo._mongoClient;
 } else {
     // In production mode, it's best to not use a global variable.
-    client = new MongoClient(uri);
+    client = new MongoClient(uri, options);
 }
 
 // Export a module-scoped MongoClient. By doing this in a
 // separate module, the client can be shared across functions.
-
 export default client;
